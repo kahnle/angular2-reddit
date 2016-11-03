782bd369e90e9269fa0b1ff5df29ccccf8dc37fe
Han Le <hanln@bravesoft.vn>
2016-11-03 17:47:04 +0700

    add new component

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from './article.model'
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
+})
+export class ArticleComponent implements OnInit {
+  @Input() article: Article;
+
+  constructor() {
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+    console.log(this.article.domain());
+  }
+
+}
